2f06c9552c64eaff22bedd1581d47fe9940b0f99
ynop <m.buechi@outlook.com>
2022-11-22 16:07:56 +0100

    tmux updates

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -5,10 +5,10 @@ ln -s $(pwd)/git/.gitignore_global $HOME/.gitignore_global
 
 ln -s $(pwd)/.alacritty.yml $HOME/.alacritty.yml
 
-ln -s $(pwd)/.tmux.conf $HOME/.tmux.conf
+ln -s $(pwd)/tmux/.tmux.conf $HOME/.tmux.conf
 
 mkdir -p $HOME/.tmuxp
-ln -s $(pwd)/tmux_code.yml $HOME/.tmuxp/tmux_code.yml
+ln -s $(pwd)/tmux/default.yml $HOME/.tmuxp/default.yml
 
 mkdir -p $HOME/.ssh
 ln -s $(pwd)/ssh/config $HOME/.ssh/config
@@ -16,6 +16,7 @@ ln -s $(pwd)/ssh/connections $HOME/.ssh/connections
 
 mkdir -p $HOME/.config/nvim
 ln -s $(pwd)/neovim/init.lua $HOME/.config/nvim/init.lua
+rm $HOME/.config/nvim/lua
 ln -s $(pwd)/neovim/lua $HOME/.config/nvim/lua
 
 rm "$HOME/Library/Application Support/Spectacle/Shortcuts.json"
