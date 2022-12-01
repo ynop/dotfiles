607772c490b1d090b74362785c437bd4d3500eaf
ynop <m.buechi@outlook.com>
2022-12-01 16:07:54 +0100

    ltex

--- neovim/lua/config/lsp.lua
+++ neovim/lua/config/lsp.lua
@@ -1,70 +1,76 @@
 local M = {}
 
 function M.setup()
-	require("mason").setup({
-		ui = {
-			icons = {
-				package_installed = "✓",
-				package_pending = "➜",
-				package_uninstalled = "✗",
-			},
-		},
-	})
+    require("mason").setup({
+        ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗",
+            },
+        },
+    })
 
-	require("mason-lspconfig").setup()
+    require("mason-lspconfig").setup()
 
-	-- Use an on_attach function to only map the following keys
-	-- after the language server attaches to the current buffer
-	local on_attach = function(client, bufnr)
-		require("keys").setup_lsp_buffer_keymaps(bufnr)
-	end
+    -- Use an on_attach function to only map the following keys
+    -- after the language server attaches to the current buffer
+    local on_attach = function(client, bufnr)
+        require("keys").setup_lsp_buffer_keymaps(bufnr)
+    end
 
-	local lsp_flags = {
-		-- This is the default in Nvim 0.7+
-		debounce_text_changes = 150,
-	}
+    local lsp_flags = {
+        -- This is the default in Nvim 0.7+
+        debounce_text_changes = 150,
+    }
 
-	-- Add additional capabilities supported by nvim-cmp
-	local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    -- Add additional capabilities supported by nvim-cmp
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-	require("lspconfig")["pyright"].setup({
-		on_attach = on_attach,
-		flags = lsp_flags,
-		capabilities = capabilities,
-	})
+    require("lspconfig")["pyright"].setup({
+        on_attach = on_attach,
+        flags = lsp_flags,
+        capabilities = capabilities,
+    })
 
-	require("lspconfig")["texlab"].setup({
-		on_attach = on_attach,
-		flags = lsp_flags,
-		capabilities = capabilities,
-	})
+    require("lspconfig")["texlab"].setup({
+        on_attach = on_attach,
+        flags = lsp_flags,
+        capabilities = capabilities,
+    })
 
-	require("lspconfig")["sumneko_lua"].setup({
-		on_attach = on_attach,
-		flags = lsp_flags,
-		capabilities = capabilities,
-		settings = {
-			Lua = {
-				runtime = {
-					-- Tell the ls which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-					version = "LuaJIT",
-				},
-				diagnostics = {
-					-- Get the language server to recognize the `vim` global
-					globals = { "vim" },
-				},
-				workspace = {
-					-- Make the server aware of Neovim runtime files
-					library = vim.api.nvim_get_runtime_file("", true),
-					checkThirdParty = false,
-				},
-				-- Do not send telemetry data containing a randomized but unique identifier
-				telemetry = {
-					enable = false,
-				},
-			},
-		},
-	})
+    require("lspconfig")["sumneko_lua"].setup({
+        on_attach = on_attach,
+        flags = lsp_flags,
+        capabilities = capabilities,
+        settings = {
+            Lua = {
+                runtime = {
+                    -- Tell the ls which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                    version = "LuaJIT",
+                },
+                diagnostics = {
+                    -- Get the language server to recognize the `vim` global
+                    globals = { "vim" },
+                },
+                workspace = {
+                    -- Make the server aware of Neovim runtime files
+                    library = vim.api.nvim_get_runtime_file("", true),
+                    checkThirdParty = false,
+                },
+                -- Do not send telemetry data containing a randomized but unique identifier
+                telemetry = {
+                    enable = false,
+                },
+            },
+        },
+    })
+
+    require("lspconfig")["ltex"].setup({
+        on_attach = on_attach,
+        flags = lsp_flags,
+        capabilities = capabilities,
+    })
 end
 
 return M
--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -47,7 +47,9 @@ return require("packer").startup(function(use)
     use({
         "nvim-telescope/telescope.nvim",
         tag = "0.1.0",
-        requires = { { "nvim-lua/plenary.nvim" } },
+        requires = {
+            "nvim-lua/plenary.nvim",
+        },
         config = function()
             require("keys").setup_telescope_keymaps()
         end,
