53b84c2764eed77d1b4ebc97bfafc83d7f41fdad
ynop <m.buechi@outlook.com>
2022-12-01 15:36:06 +0100

    keymaps

--- neovim/lua/keys.lua
+++ neovim/lua/keys.lua
@@ -2,158 +2,170 @@ local M = {}
 local map = vim.api.nvim_set_keymap
 
 function M.setup_main_keymaps()
-	local legend = require("legendary")
-	local toolbox = require("legendary.toolbox")
+    local legend = require("legendary")
+    local toolbox = require("legendary.toolbox")
 
-	map("i", "jk", "<Esc>", {})
+    map("i", "jk", "<Esc>", {})
 
-	local diagnostic_opts = { noremap = true, silent = true }
-	legend.keymaps({
-		-- General
-		{ "//", { v = 'y/<C-R>"<CR>' }, description = "search for selected text" },
-		{ "<leader>c", { v = '"*y' }, description = "copy to system clipboard" },
-		-- Diagnostics
-		{
-			"<leader>e",
-			vim.diagnostic.open_float,
-			description = "show diagnostics in floating window",
-			diagnostic_opts,
-		},
-		{ "[d", vim.diagnostic.goto_prev, description = "goto next diagnostic", diagnostic_opts },
-		{ "]d", vim.diagnostic.goto_next, description = "goto previous diagnostic", diagnostic_opts },
-		-- LSP
-		{
-			"fc",
-			toolbox.lazy(vim.lsp.buf.format, { timeout_ms = 2000 }),
-			description = "format current buffer",
-		},
-	})
+    local diagnostic_opts = { noremap = true, silent = true }
+    legend.keymaps({
+        -- General
+        { "//", { v = 'y/<C-R>"<CR>' }, description = "search for selected text" },
+        -- Diagnostics
+        {
+            "<leader>e",
+            vim.diagnostic.open_float,
+            description = "show diagnostics in floating window",
+            diagnostic_opts,
+        },
+        { "[d", vim.diagnostic.goto_prev, description = "goto next diagnostic", diagnostic_opts },
+        { "]d", vim.diagnostic.goto_next, description = "goto previous diagnostic", diagnostic_opts },
+        -- LSP
+        {
+            "<leader>f",
+            toolbox.lazy(vim.lsp.buf.format, { timeout_ms = 2000 }),
+            description = "format current buffer",
+        },
+    })
 end
 
 function M.setup_legendary_keymaps()
-	local legend = require("legendary")
-	legend.keymaps({
-		{ "<leader>?", ":Legendary<CR>", description = "search keymaps, commands, autocmds" },
-	})
+    local legend = require("legendary")
+    legend.keymaps({
+        { "<leader>?", ":Legendary<CR>", description = "search keymaps, commands, autocmds" },
+    })
 end
 
 function M.setup_tree_keymaps()
-	local legend = require("legendary")
-	legend.keymaps({
-		{ "<C-n>", ":NvimTreeToggle<CR>", description = "open/close file tree" },
-	})
+    local legend = require("legendary")
+    legend.keymaps({
+        { "<leader>n", ":NvimTreeToggle<CR>", description = "open/close file tree" },
+    })
 end
 
 function M.setup_telescope_keymaps()
-	local builtin = require("telescope.builtin")
-	local legend = require("legendary")
-	legend.keymaps({
-		{ "<leader>ff", builtin.find_files, description = "find files in CWD" },
-		{ "<leader>fg", builtin.live_grep, description = "search for a string in CWD" },
-		{ "<leader>fb", builtin.buffers, description = "find buffer" },
-		{ "<leader>fh", builtin.help_tags, description = "find help tags" },
-	})
+    local builtin = require("telescope.builtin")
+    local legend = require("legendary")
+    local toolbox = require("legendary.toolbox")
+
+    legend.keymaps({
+        { "<leader>m", ":Telescope<CR>", description = "open telescope" },
+        { "<leader>ff", builtin.find_files, description = "find files in CWD" },
+        { "<leader>fg", builtin.live_grep, description = "search for a string in CWD" },
+        { "<leader>fb", builtin.buffers, description = "find buffer" },
+        { "<leader>fh", builtin.help_tags, description = "find help tags" },
+        { "<leader>fq", builtin.quickfix, description = "find items in quickfix list" },
+        { "<leader>fl", builtin.loclist, description = "find items in location list" },
+        { "<leader>fr", builtin.loclist, description = "find register" },
+        {
+            "<leader>fd",
+            toolbox.lazy(builtin.diagnostics, { bufnr = 0 }),
+            description = "find diagnostics in current buffer",
+        },
+        { "<leader>fw", builtin.diagnostics, description = "find diagnostics in workspace" },
+    })
 end
 
 function M.setup_trouble_keymaps()
-	local legend = require("legendary")
-	legend.keymaps({
-		{ "<leader>ot", ":TroubleToggle<CR>", description = "toggle trouble" },
-		{ "<leader>ow", ":TroubleToggle workspace_diagnostics<CR>", description = "toggle lsp workspace diagnostics" },
-		{ "<leader>od", ":TroubleToggle document_diagnostics<CR>", description = "toggle lsp document diagnostics" },
-		{ "<leader>oq", ":TroubleToggle quickfix<CR>", description = "toggle quickfix" },
-		{ "<leader>ol", ":TroubleToggle loclist<CR>", description = "toggle loclist" },
-	})
+    local legend = require("legendary")
+    legend.keymaps({
+        { "<leader>ot", ":TroubleToggle<CR>", description = "toggle trouble" },
+        { "<leader>ow", ":TroubleToggle workspace_diagnostics<CR>", description = "toggle lsp workspace diagnostics" },
+        { "<leader>od", ":TroubleToggle document_diagnostics<CR>", description = "toggle lsp document diagnostics" },
+        { "<leader>oq", ":TroubleToggle quickfix<CR>", description = "toggle quickfix" },
+        { "<leader>ol", ":TroubleToggle loclist<CR>", description = "toggle loclist" },
+    })
 end
 
 function M.setup_bufferline_keymaps()
-	local legend = require("legendary")
-	legend.keymaps({
-		{ "<leader>h", "<Plug>(cokeline-focus-prev)", description = "move to previous buffer" },
-		{ "<leader>l", "<Plug>(cokeline-focus-next)", description = "move to next buffer" },
-		{ "<leader>b", "<Plug>(cokeline-pick-focus)", description = "move to buffer by index" },
-		{ "<leader>x", "<Plug>(cokeline-pick-close)", description = "close buffer by index" },
-	})
+    local legend = require("legendary")
+    legend.keymaps({
+        { "<leader>h", "<Plug>(cokeline-focus-prev)", description = "move to previous buffer" },
+        { "<leader>l", "<Plug>(cokeline-focus-next)", description = "move to next buffer" },
+        { "<leader>b", "<Plug>(cokeline-pick-focus)", description = "move to buffer by index" },
+        { "<leader>x", "<Plug>(cokeline-pick-close)", description = "close buffer by index" },
+    })
 end
 
 function M.setup_testing_keymaps()
-	local legend = require("legendary")
-	local neotest = require("neotest")
-	local testing = require("config.testing")
-	legend.keymaps({
-		{ "<leader>tn", neotest.run.run, description = "run nearest test" },
-		{ "<leader>tf", testing.run_on_current_file, description = "run tests in current file" },
-		{ "<leader>ta", neotest.run.attach, description = "attach to the nearest test" },
-		{ "<leader>ts", neotest.summary.toggle, description = "show test summary window" },
-		{ "<leader>to", neotest.output.open, description = "show output of test" },
-		{ "<leader>tp", neotest.output_panel.toggle, description = "show test outputs in window" },
-	})
+    local legend = require("legendary")
+    local neotest = require("neotest")
+    local testing = require("config.testing")
+    legend.keymaps({
+        { "<leader>tn", neotest.run.run, description = "run nearest test" },
+        { "<leader>tf", testing.run_on_current_file, description = "run tests in current file" },
+        { "<leader>ta", neotest.run.attach, description = "attach to the nearest test" },
+        { "<leader>ts", neotest.summary.toggle, description = "show test summary window" },
+        { "<leader>to", neotest.output.open, description = "show output of test" },
+        { "<leader>tp", neotest.output_panel.toggle, description = "show test outputs in window" },
+    })
 end
 
 function M.setup_debug_keymaps()
-	local legend = require("legendary")
-	local dap = require("dap")
-	local debugging = require("config.debugging")
-	legend.keymaps({
-		{ "<leader>dc", dap.continue, description = "debug continue/run" },
-		{ "<leader>dl", dap.run_last, description = "debug last" },
-		{ "<leader>dt", dap.toggle_breakpoint, description = "toggle debug breakpoint" },
-		{ "<leader>di", dap.step_into, description = "debug step into" },
-		{ "<leader>do", dap.step_over, description = "debug step over" },
-		{ "<leader>ds", debugging.view_scopes, description = "open debug scopes in window" },
-		{ "<leader>df", debugging.view_frames, description = "open debug frames in window" },
-		{ "<leader>dh", debugging.hover, description = "show debug info in floating window" },
-		{ "<leader>dr", dap.repl.open, description = "inspect debug state with built-in REPL" },
-	})
+    local legend = require("legendary")
+    local dap = require("dap")
+    local debugging = require("config.debugging")
+    legend.keymaps({
+        { "<leader>dc", dap.continue, description = "debug continue/run" },
+        { "<leader>dl", dap.run_last, description = "debug last" },
+        { "<leader>dt", dap.toggle_breakpoint, description = "toggle debug breakpoint" },
+        { "<leader>di", dap.step_into, description = "debug step into" },
+        { "<leader>do", dap.step_over, description = "debug step over" },
+        { "<leader>ds", debugging.view_scopes, description = "open debug scopes in window" },
+        { "<leader>df", debugging.view_frames, description = "open debug frames in window" },
+        { "<leader>dh", debugging.hover, description = "show debug info in floating window" },
+        { "<leader>dr", dap.repl.open, description = "inspect debug state with built-in REPL" },
+    })
 end
 
 function M.setup_split_keymaps()
-	local legend = require("legendary")
-	local ss = require("smart-splits")
+    local legend = require("legendary")
+    local ss = require("smart-splits")
 
-	legend.keymaps({
-		{ "<C-K>", ss.move_cursor_up, description = "Move to split above" },
-		{ "<C-J>", ss.move_cursor_down, description = "Move to split below" },
-		{ "<C-H>", ss.move_cursor_left, description = "Move to split to the left" },
-		{ "<C-L>", ss.move_cursor_right, description = "Move to split to the right" },
-		{ "\x1bk", ss.resize_up, description = "Resize split at the top" },
-		{ "\x1bj", ss.resize_down, description = "Resize split at the bottom" },
-		{ "\x1bh", ss.resize_left, description = "Resize split at the left side" },
-		{ "\x1bl", ss.resize_right, description = "Resize split at the right side" },
-		{ "<leader>s", ss.start_resize_mode, description = "Start split resize mode" },
-	})
+    legend.keymaps({
+        { "<C-K>", ss.move_cursor_up, description = "Move to split above" },
+        { "<C-J>", ss.move_cursor_down, description = "Move to split below" },
+        { "<C-H>", ss.move_cursor_left, description = "Move to split to the left" },
+        { "<C-L>", ss.move_cursor_right, description = "Move to split to the right" },
+        { "\x1bk", ss.resize_up, description = "Resize split at the top" },
+        { "\x1bj", ss.resize_down, description = "Resize split at the bottom" },
+        { "\x1bh", ss.resize_left, description = "Resize split at the left side" },
+        { "\x1bl", ss.resize_right, description = "Resize split at the right side" },
+        { "<C-S>", ss.start_resize_mode, description = "Start split resize mode" },
+    })
 end
 
 function M.setup_lsp_buffer_keymaps(bufnr)
-	local legend = require("legendary")
-	local toolbox = require("legendary.toolbox")
+    local legend = require("legendary")
+    local toolbox = require("legendary.toolbox")
+    local builtin = require("telescope.builtin")
 
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-	legend.keymaps({
-		{ "gd", vim.lsp.buf.definition, description = "LSP go to definition", opts = bufopts },
-		{ "gc", vim.lsp.buf.declaration, description = "LSP go to declaration", opts = bufopts },
-		{ "K", vim.lsp.buf.hover, description = "LSP hover", opts = bufopts },
-		{ "gi", vim.lsp.buf.implementation, description = "LSP goto implementation", opts = bufopts },
-		{ "gh", vim.lsp.buf.signature_help, description = "LSP show signature help", opts = bufopts },
-		{ "<leader>wa", vim.lsp.buf.add_workspace_folder, description = "LSP add workspace folder", opts = bufopts },
-		{
-			"<leader>wr",
-			vim.lsp.buf.remove_workspace_folder,
-			description = "LSP remove workspace folder",
-			opts = bufopts,
-		},
-		{
-			"<leader>wl",
-			toolbox.lazy(vim.inspect, vim.lsp.buf.list_workspace_folders()),
-			description = "LSP list workspace folders",
-			opts = bufopts,
-		},
-		{ "gt", vim.lsp.buf.type_definition, description = "LSP go to type definition", opts = bufopts },
-		{ "<leader>rn", vim.lsp.buf.rename, description = "LSP rename", opts = bufopts },
-		{ "<leader>ca", vim.lsp.buf.code_action, description = "LSP code action", opts = bufopts },
-		{ "gr", vim.lsp.buf.references, description = "LSP list references", opts = bufopts },
-	})
+    legend.keymaps({
+        { "gd", builtin.lsp_definitions, description = "LSP go to definition", opts = bufopts },
+        { "gD", vim.lsp.buf.declaration, description = "LSP go to declaration", opts = bufopts },
+        { "gi", builtin.lsp_implementations, description = "LSP goto implementation", opts = bufopts },
+        { "gh", vim.lsp.buf.signature_help, description = "LSP show signature help", opts = bufopts },
+        { "gt", builtin.type_definitions, description = "LSP go to type definition", opts = bufopts },
+        { "gr", builtin.lsp_references, description = "LSP list references", opts = bufopts },
+        { "<leader>k", vim.lsp.buf.hover, description = "LSP hover", opts = bufopts },
+        { "<leader>rn", vim.lsp.buf.rename, description = "LSP rename", opts = bufopts },
+        { "<leader>ca", vim.lsp.buf.code_action, description = "LSP code action", opts = bufopts },
+        { "<leader>wa", vim.lsp.buf.add_workspace_folder, description = "LSP add workspace folder", opts = bufopts },
+        {
+            "<leader>wr",
+            vim.lsp.buf.remove_workspace_folder,
+            description = "LSP remove workspace folder",
+            opts = bufopts,
+        },
+        {
+            "<leader>wl",
+            toolbox.lazy(vim.inspect, vim.lsp.buf.list_workspace_folders()),
+            description = "LSP list workspace folders",
+            opts = bufopts,
+        },
+    })
 end
 
 return M
--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -1,180 +1,180 @@
 return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
+    use("wbthomason/packer.nvim")
 
-	-- Colorscheme
-	use({
-		"morhetz/gruvbox",
-		config = function()
-			vim.cmd("colorscheme gruvbox")
-		end,
-	})
+    -- Colorscheme
+    use({
+        "morhetz/gruvbox",
+        config = function()
+            vim.cmd("colorscheme gruvbox")
+        end,
+    })
 
-	-- UI
-	use({ "stevearc/dressing.nvim" })
+    -- UI
+    use({ "stevearc/dressing.nvim" })
 
-	-- Legendary
-	use({
-		"mrjones2014/legendary.nvim",
-		config = function()
-			require("legendary").setup()
-			require("keys").setup_legendary_keymaps()
-		end,
-	})
+    -- Legendary
+    use({
+        "mrjones2014/legendary.nvim",
+        config = function()
+            require("legendary").setup()
+            require("keys").setup_legendary_keymaps()
+        end,
+    })
 
-	-- Treesitter
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-		config = function()
-			require("config.treesitter").setup()
-		end,
-	})
+    -- Treesitter
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+        config = function()
+            require("config.treesitter").setup()
+        end,
+    })
 
-	-- nvim tree
-	use({
-		"nvim-tree/nvim-tree.lua",
-		requires = {
-			"nvim-tree/nvim-web-devicons", -- optional, for file icons
-		},
-		tag = "nightly", -- optional, updated every week. (see issue #1193)
-		config = function()
-			require("keys").setup_tree_keymaps()
-			require("nvim-tree").setup({})
-		end,
-	})
+    -- nvim tree
+    use({
+        "nvim-tree/nvim-tree.lua",
+        requires = {
+            "nvim-tree/nvim-web-devicons", -- optional, for file icons
+        },
+        tag = "nightly", -- optional, updated every week. (see issue #1193)
+        config = function()
+            require("keys").setup_tree_keymaps()
+            require("nvim-tree").setup({})
+        end,
+    })
 
-	-- telescope
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
-		requires = { { "nvim-lua/plenary.nvim" } },
-		config = function()
-			require("keys").setup_telescope_keymaps()
-		end,
-	})
+    -- telescope
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.0",
+        requires = { { "nvim-lua/plenary.nvim" } },
+        config = function()
+            require("keys").setup_telescope_keymaps()
+        end,
+    })
 
-	-- lualine
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-		config = function()
-			require("lualine").setup({})
-		end,
-	})
+    -- lualine
+    use({
+        "nvim-lualine/lualine.nvim",
+        requires = { "kyazdani42/nvim-web-devicons", opt = true },
+        config = function()
+            require("lualine").setup({})
+        end,
+    })
 
-	-- Formatting/Linting
-	use({
-		"jose-elias-alvarez/null-ls.nvim",
-		requires = { { "nvim-lua/plenary.nvim" } },
-		config = function()
-			require("config.nullls").setup()
-		end,
-	})
+    -- Formatting/Linting
+    use({
+        "jose-elias-alvarez/null-ls.nvim",
+        requires = { { "nvim-lua/plenary.nvim" } },
+        config = function()
+            require("config.nullls").setup()
+        end,
+    })
 
-	-- Auto Completion and Snippets
-	use({
-		"hrsh7th/nvim-cmp",
-		config = function()
-			require("config.cmp").setup()
-		end,
-		requires = {
-			"hrsh7th/cmp-nvim-lsp",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-			"hrsh7th/cmp-path",
-		},
-	})
+    -- Auto Completion and Snippets
+    use({
+        "hrsh7th/nvim-cmp",
+        config = function()
+            require("config.cmp").setup()
+        end,
+        requires = {
+            "hrsh7th/cmp-nvim-lsp",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+            "hrsh7th/cmp-path",
+        },
+    })
 
-	-- LSP
-	use({
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("config.lsp").setup()
-		end,
-		requires = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-		},
-	})
+    -- LSP
+    use({
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("config.lsp").setup()
+        end,
+        requires = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+        },
+    })
 
-	-- Latex
-	use({
-		"lervag/vimtex",
-		config = function()
-			require("config.tex").setup()
-		end,
-	})
+    -- Latex
+    use({
+        "lervag/vimtex",
+        config = function()
+            require("config.tex").setup()
+        end,
+    })
 
-	-- Bufferline
-	use({
-		"noib3/nvim-cokeline",
-		requires = "kyazdani42/nvim-web-devicons",
-		config = function()
-			require("config.bufferline").setup()
-			require("keys").setup_bufferline_keymaps()
-		end,
-	})
+    -- Bufferline
+    use({
+        "noib3/nvim-cokeline",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function()
+            require("config.bufferline").setup()
+            require("keys").setup_bufferline_keymaps()
+        end,
+    })
 
-	-- Trouble
-	use({
-		"folke/trouble.nvim",
-		requires = "kyazdani42/nvim-web-devicons",
-		config = function()
-			require("trouble").setup()
-			require("keys").setup_trouble_keymaps()
-		end,
-	})
+    -- Trouble
+    use({
+        "folke/trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function()
+            require("trouble").setup()
+            require("keys").setup_trouble_keymaps()
+        end,
+    })
 
-	-- Testing
-	use({
-		"nvim-neotest/neotest",
-		requires = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"antoinemadec/FixCursorHold.nvim",
-			"nvim-neotest/neotest-python",
-			"nvim-neotest/neotest-plenary",
-		},
-		config = function()
-			require("config.testing").setup()
-			require("keys").setup_testing_keymaps()
-		end,
-	})
+    -- Testing
+    use({
+        "nvim-neotest/neotest",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "antoinemadec/FixCursorHold.nvim",
+            "nvim-neotest/neotest-python",
+            "nvim-neotest/neotest-plenary",
+        },
+        config = function()
+            require("config.testing").setup()
+            require("keys").setup_testing_keymaps()
+        end,
+    })
 
-	-- Debugging
-	use({
-		"mfussenegger/nvim-dap",
-		requires = {
-			"mfussenegger/nvim-dap-python",
-		},
-		config = function()
-			require("config.debugging").setup()
+    -- Debugging
+    use({
+        "mfussenegger/nvim-dap",
+        requires = {
+            "mfussenegger/nvim-dap-python",
+        },
+        config = function()
+            require("config.debugging").setup()
             require("keys").setup_debug_keymaps()
-		end,
-	})
+        end,
+    })
 
-	-- Project level settings
-	use({
-		"MunifTanjim/exrc.nvim",
-		requires = {
-			"MunifTanjim/nui.nvim",
-		},
-		config = function()
-			require("config.localconfig").setup()
-		end,
-	})
+    -- Project level settings
+    use({
+        "MunifTanjim/exrc.nvim",
+        requires = {
+            "MunifTanjim/nui.nvim",
+        },
+        config = function()
+            require("config.localconfig").setup()
+        end,
+    })
 
-	-- Editorconfig
-	use({
-		"gpanders/editorconfig.nvim",
-	})
+    -- Editorconfig
+    use({
+        "gpanders/editorconfig.nvim",
+    })
 
-	-- Splits
-	use({
-		"mrjones2014/smart-splits.nvim",
-		config = function()
-			require("keys").setup_split_keymaps()
-		end,
-	})
+    -- Splits
+    use({
+        "mrjones2014/smart-splits.nvim",
+        config = function()
+            require("keys").setup_split_keymaps()
+        end,
+    })
 end)
