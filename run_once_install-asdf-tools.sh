5b1ea79b2872ef3365ff4fb9d3f3ec8f41608a8f
ynop <m.buechi@outlook.com>
2022-12-04 23:25:52 +0100

    asdf add go

--- run_once_install-asdf-tools.sh
+++ run_once_install-asdf-tools.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+# .tool-versions hash: {{ include "~/.tool-versions" | sha256sum }}
+asdf plugin add Python
+asdf plugin add nodejs
+asdf plugin add java
+asdf plugin add lua
+asdf plugin add golang
+
 asdf install
