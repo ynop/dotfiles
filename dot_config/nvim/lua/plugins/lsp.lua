d8e4229186422e47e427fc37292a545abe1e3547
ynop <m.buechi@outlook.com>
2023-09-27 15:21:24 +0200

    pyright config

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,1 +1,28 @@
-return {}
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      pyright = {
+        settings = {
+          python = {
+            analysis = {
+              autoImportCompletion = true,
+              diagnosticMode = "workspace",
+            },
+          },
+        },
+      },
+      ruff_lsp = {},
+    },
+    setup = {
+      ruff_lsp = function()
+        require("lazyvim.util").on_attach(function(client, _)
+          if client.name == "ruff_lsp" then
+            -- Disable hover in favor of Pyright
+            client.server_capabilities.hoverProvider = false
+          end
+        end)
+      end,
+    },
+  },
+}
