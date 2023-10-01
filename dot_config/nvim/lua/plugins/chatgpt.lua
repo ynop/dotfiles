2ed19893217adff3ee63b694ea6c42dc9af028fc
ynop <m.buechi@outlook.com>
2023-10-01 16:10:37 +0200

    ChatGPT, Codeium

---
+++ dot_config/nvim/lua/plugins/chatgpt.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "folke/which-key.nvim",
+    optional = false,
+    opts = {
+      defaults = {
+        ["<leader>a"] = { name = "+AI" },
+      },
+    },
+  },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup()
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    keys = {
+      { "<leader>ac", "<cmd>ChatGPT<CR>", desc = "ChatGPT" },
+      { "<leader>ae", "<cmd>ChatGPTEditWithInstruction<CR>", mode = { "n", "v" }, desc = "ChatGPTEditWithInstruction" },
+      { "<leader>at", "<cmd>ChatGPTRun translate<CR>", mode = { "n", "v" }, desc = "translate (ChatGPT)" },
+      { "<leader>ad", "<cmd>ChatGPTRun docstring<CR>", mode = { "n", "v" }, desc = "docstring (ChatGPT)" },
+      {
+        "<leader>ag",
+        "<cmd>ChatGPTRun grammar_correction<CR>",
+        mode = { "n", "v" },
+        desc = "grammar correction (ChatGPT)",
+      },
+      { "<leader>ap", "<cmd>ChatGPTRun add_tests<CR>", mode = { "n", "v" }, desc = "add tests (ChatGPT)" },
+    },
+  },
+}
