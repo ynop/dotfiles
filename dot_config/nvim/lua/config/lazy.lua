a291f4bf564eee8dca75ff63103b25e49b726d7f
matthias <matthias.buechi@identech.ch>
2023-10-29 21:47:22 +0100

    cleanup

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -12,7 +12,6 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.dap.core" },
-    { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
